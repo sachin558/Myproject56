3a686e775f5a4ca3a4a1f6c8870541a062ad16f8
Sachin <sachin.kumar_cs20@gla.ac.in>
2022-01-15 14:40:16 +0530

    Sum_of_index2_t0_8

--- src/Sum_of_index2_t0_8.java
+++ src/Sum_of_index2_t0_8.java
@@ -1,28 +1,28 @@
 import java.util.Scanner;
 
 /*
-Take the input form the user in the array form and find the sum of the odd index
+Take the input form the user in the array form and find the sum of the index greater then 2 and less then 8
  */
-public class Sum_of_odd_index {
+public class Sum_of_index2_t0_8 {
     int arr[],i,sum=0;
-    Sum_of_odd_index(){
+    Sum_of_index2_t0_8(){
         Scanner sc=new Scanner(System.in);
         System.out.println("enter the size of the array ");
         arr=new int[sc.nextInt()];
         System.out.println("enter  the element  of the array ");
-        for ( i = 1; i < arr.length; i++) {
+        for ( i = 0; i < arr.length; i++) {
             arr[i]=sc.nextInt();
         }
     }void display(){
-        for ( i = 1; i < arr.length; i++) {
-            if(i%2!=0){
+        for ( i = 0; i < arr.length; i++) {
+            if(i>2&&i<8){
                 sum+=arr[i];
             }
         }
         System.out.println(sum);
     }
     public static void main(String[] args) {
-        Sum_of_odd_index obj=new Sum_of_odd_index();
+        Sum_of_index2_t0_8 obj=new Sum_of_index2_t0_8();
         obj.display();
     }
 }
