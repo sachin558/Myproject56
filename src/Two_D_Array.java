5d26946e7c36d9d1567a725fdb26a8a59e5da59f
Sachin <sachin.kumar_cs20@gla.ac.in>
2022-01-21 15:54:13 +0530

    even and odd number count

--- src/Two_D_Array.java
+++ src/Two_D_Array.java
@@ -1,32 +1,44 @@
+import java.sql.SQLSyntaxErrorException;
+import java.util.ArrayList;
 import java.util.Scanner;
 /*
-write the program to the find the target values in the Two array ;
+Write a program to input the elements of a two- dimensional array. Then from this array, make two arrays one that stores all odd elements of the two-dimensional array and the other that stores all even elements of the array. 
+Note:-  Resulting arrays will be of ArrayList type
  */
 public class Two_D_Array {
-    int arr[][],i,j,sum=0;
+    int marks[][],i,j,values,c,d;
     Two_D_Array(){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("entre the size of the array and columns size of the  ");
-        arr=new int[sc.nextInt()][sc.nextInt()];
-        System.out.println("enter the element of the array ");
-        for (i= 0; i <arr.length ; i++) {
-            for (j = 0;  j< arr.length; j++) {
-                arr[i][j]=sc.nextInt();
+        Scanner sc=new Scanner(System.in );
+        System.out.println("enter  the size of the array");
+        marks=new int[sc.nextInt()][sc.nextInt()];
+        System.out.println("enter the target value");
+        values=sc.nextInt();
+        System.out.println("enter the element  of the array ");
+        for ( i = 0; i < marks.length; i++) {
+            for (j= 0; j < marks.length; j++) {
+                marks[i][j]=sc.nextInt();
             }
         }
-    }
-    void display(){
-        for (i= 0; i <arr.length ; i++) {
-            for (j = 0;  j< arr.length; j++) {
-                sum+=arr[i][j];
-                System.out.print(arr[i][j]+" ");
+    }void display(){
+        for (i = 0;  i< marks.length; i++) {
+            for (j= 0; j< marks[i].length; j++) {
+                if(marks[i][j]%2==0){
+                    System.out.print(marks[i][j]+" ");
+                    c++;
+                }else {
+                    System.out.println("this the odd number  "+marks[i][j]+"");
+                    d++;
+                }
+
             }
             System.out.println();
         }
-        System.out.println(sum);
+        System.out.println();
+        System.out.println("this is total number tof the even number "+c+" and this is total number of the odd number "+d);
     }
     public static void main(String[] args) {
         Two_D_Array obj=new Two_D_Array();
-        obj.display();
+        ArrayList<Two_D_Array> obj1=new ArrayList<>();
     }
+
 }
