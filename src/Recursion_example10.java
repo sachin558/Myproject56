3562ad06bc52ec48dba8899a224e6546b9bf1a96
Sachin <sachin.kumar_cs20@gla.ac.in>
2022-01-30 08:41:34 +0530

    commit

--- src/Recursion_example10.java
+++ src/Recursion_example10.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
  Create an array of 5 elements with values {1,2,4,2,6}.search value 2 in the given array and print the first occurrence(index)
  of search value. Do it recursively
@@ -9,12 +11,18 @@ public static int find(int arr[ ], int currentIndex, int search_value)
 
  */
 public class Recursion_example10 {
-    public static int find(int arr[],int currentindex,int search_value){
-        if (currentindex<0){
+    public static int find(int arr[],int n,int search){
+        if(n==0){
             return 0;
         }
-        if(arr[currentindex]==2){
-            return arr;
-        }
+        if(arr[n]==search)
+            return search;
+        System.out.println(search);
+        return find(arr,n-1,search);
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,2};
+        int n=arr.length;
+        find(arr,n,2);
     }
 }
