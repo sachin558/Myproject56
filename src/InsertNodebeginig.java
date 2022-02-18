9b26e6e23162682170296590e81e786ca41913a6
Sachin <sachin.kumar_cs20@gla.ac.in>
2022-02-18 20:11:52 +0530

    insert_node_check_the_data_value_is_even_odd

---
+++ src/InsertNodebeginig.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class InsertNodebeginig {
+    Node head;
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+        public void inser(int data){
+            Node newnode=new Node(data);
+            newnode.next=head;
+            head=newnode;
+        }
+        public void display(){
+            Node temp=head;
+            while (temp!=null){
+//                System.out.print(temp.data+" ");
+                if(temp.data%2==0){
+                    System.out.println("even number "+temp.data);
+                }else {
+                    System.out.println("odd number "+temp.data);
+                }
+                temp=temp.next;
+            }
+        }
+
+    public static void main(String[] args) {
+        InsertNodebeginig obj=new InsertNodebeginig();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the data  value");
+        obj.inser(sc.nextInt());
+        obj.inser(sc.nextInt());
+        obj.inser(sc.nextInt());
+        obj.display();
+    }
+    }
+
+
